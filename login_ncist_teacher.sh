43171bf926125e824f7f675bad3fd73ccdc60654
xiadengma <xiadengma@gmail.com>
2021-11-27 16:10:51 +0800

    修复检测脚本路径错误的问题

--- login_ncist_teacher.sh
+++ login_ncist_teacher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if   [[  $(nmcli -t -f NAME connection show --active) == "ncist-teacher"  ]];
 then 
-  /home/xiadengma/miniconda3/envs/auto-ncist-teacher/bin/python /home/xiadengma/code/个人项目/python/auto-ncist-teacher/auto_2.py
+  /home/xiadengma/miniconda3/envs/auto-ncist-teacher/bin/python /home/xiadengma/code/个人项目/python/NCIST-Drcom-Auto/auto_2.py
 fi
